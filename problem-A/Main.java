189f71f214ade52fcffccfcfdb41c53ba48bb71b
220103306 <125125442+220103306@users.noreply.github.com>
2023-02-26 01:23:54 +0600

    Create Main.java

---
+++ problem-A/Main.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String[] args){
+        MovablePoint myPoint = new MovablePoint(2, 2 , 1, 1);
+        MovableCircle myCircle = new MovableCircle(2, 2, 1, 1, 3);
+        System.out.println(myPoint.toString());
+        System.out.println(myCircle.toString());
+
+    }
+}
