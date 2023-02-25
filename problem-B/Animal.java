6a369925a0eb4031cd0efbe6811e834c29a510cd
220103306 <125125442+220103306@users.noreply.github.com>
2023-02-26 01:25:25 +0600

    Update Animal.java

---
+++ problem-B/Animal.java
@@ -0,0 +1,15 @@
+class Animal {
+     int legs;
+
+        protected Animal(int legs) {
+            this.legs = legs;
+        }
+
+        public void walk() {
+            System.out.println("This animal walks on " + legs + " legs");
+        }
+
+    public void eat() {
+
+    }
+}
